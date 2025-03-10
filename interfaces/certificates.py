c2567acc5ecf760afe6a5e532c9927cee5b3b3a7
John Ferry Lagman <you@example.com>
2025-03-10 23:56:54 +0800

    feat: certificates

--- interfaces/certificates.py
+++ interfaces/certificates.py
@@ -1,7 +1,61 @@
 import streamlit as st
 import services.sheets_service as ss
+import utils.certificates as ct
 
 def certificates_page():
-    st.header("Download you Certificates", )
-    get_certificates = ss.get_data_df("certificates")
-    st.dataframe(get_certificates)
+    _, middle, _ = st.columns(spec=[1,2,1])
+
+    with middle:   
+        st.header("Download your certificates here")
+        
+        email_input = st.text_input("Enter the email that you entered on the evaluation forms below: ")
+        
+        if st.button("Enter", type="secondary") or st.session_state.get('form_submitted', False):
+            # Set a flag to indicate the form was submitted
+            st.session_state.form_submitted = True
+            
+            event_participated_table = ss.get_data_ls_dict("event_participated")
+            event_id_participated = [event['event_id'] for event in event_participated_table if event['email'] == email_input]
+
+            if event_id_participated:    
+                certificates_table = ss.get_data_ls_dict("certificates")
+                event_title_participated = [event["title"] for event in certificates_table if event["event_id"] in event_id_participated]
+                cert_to_print = st.selectbox("Which event would you like a certificate for?", event_title_participated)
+
+                selected_cert_info = [info for info in certificates_table if info["title"] == cert_to_print]
+                selected_cert_info = selected_cert_info[0]
+
+                user_table = ss.get_data_ls_dict("users")
+                user_row = next((row for row in user_table if row["email"] == email_input), None)
+
+                # Add a "Generate Certificate" button to control when to generate/show the certificate
+                if st.button("Generate Certificate"):
+
+                    cert = ct.cert_maker(
+                        user_row["first_name"] + " " + user_row["last_name"],
+                        selected_cert_info["type"], 
+                        selected_cert_info["title"], 
+                        selected_cert_info["tech_team"], 
+                        selected_cert_info["certificate_description"], 
+                        selected_cert_info["date"], 
+                        selected_cert_info["gdg_lead_name"], 
+                        selected_cert_info["tech_team_lead"], 
+                        selected_cert_info["tech_team_colead"]
+                    )
+
+                   
+                    st.image(cert)
+
+                    cert.save("temp.jpg")
+
+                    # Add a download button
+                    with open("temp.jpg", "rb") as file:
+                        st.download_button(
+                            label="Download Certificate",
+                            data=file,
+                            file_name=user_row["last_name"] + "_" + selected_cert_info["title"] + ".jpg",
+                            mime="image/jpg",
+                            on_click="ignore"
+                        )
+            else:
+                st.warning('You have not yet participated in any of our events', icon="ℹ️")
---
+++ utils/certificates.py
@@ -0,0 +1,65 @@
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+
+def draw_text(draw, text, x, y, width, font_size, font_name, multiline=False):
+    scale = 4
+    x *= scale
+    y *= scale
+    width *= scale
+    font_size *= scale
+
+    # Font paths
+    font = {
+        "bold": r"static\fonts\GoogleSans-Bold.ttf",
+        "bold_italic": r"static\fonts\GoogleSans-BoldItalic.ttf",
+        "italic": r"static\fonts\GoogleSans-Italic.ttf",
+        "medium": r"static\fonts\GoogleSans-Medium.ttf",
+        "medium_italic": r"static\fonts\GoogleSans-MediumItalic.ttf",
+        "regular": r"static\fonts\GoogleSans-Regular.ttf"
+    }
+
+    font = ImageFont.truetype(font[font_name], font_size)
+
+    if multiline:
+        # Wrap text to fit within the given width
+        max_line_char = width // (font_size // 2) # Based on the assumption that the width of a character is half of its height
+        wrapped_text = textwrap.fill(text, width=max_line_char)
+        lines = wrapped_text.split("\n")
+        line_height = font_size + 5 
+
+        for i, line in enumerate(lines):
+            text_bbox = draw.textbbox((0,0), line, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            x_centered = x + (width - text_width) // 2
+            draw.text((x_centered, y + i * line_height), line, font=font, fill=(255,255,255))
+    else:
+        text_bbox = draw.textbbox((0, 0), text, font=font)
+        text_width = text_bbox[2] - text_bbox[0]
+        x_centered = x + (width - text_width) // 2
+        draw.text((x_centered, y), text, font=font, fill=(255, 255, 255)) 
+
+    
+def cert_maker(participant_name, event_type, event_title, department, event_description, event_date, gdg_lead, lead_name, colead_name):
+    # Load the template
+    img = Image.open(r"static\images\template_certificate.png").convert("RGB")
+
+    # Create a draw object to the template
+    draw = ImageDraw.Draw(img)
+
+    # Position can be derived from Figma
+    draw_text(draw, participant_name, 85, 258, 623, 48, "bold", multiline=False)
+    draw_text(draw, "has successfully participated in the " + event_type, 200, 356, 402, 12, "medium", multiline=False)
+    draw_text(draw, event_title, 6, 379, 780, 20, "bold", multiline=False)
+    draw_text(draw, "organized by the " + department, 256, 411, 282, 12, "medium", multiline=False)
+    draw_text(draw, event_description, 136, 437, 520, 10, "medium", multiline=True)
+    draw_text(draw, "Awarded on " + event_date, 296, 487, 201, 13, "medium", multiline=False)
+    draw_text(draw,  gdg_lead, 154, 534, 133, 13, "bold", multiline=False)
+    draw_text(draw,  "GDG PUP President and Chapter Lead" , 170, 550, 102, 8, "medium", multiline=True)
+    draw_text(draw,  lead_name, 292, 537, 198, 13, "bold", multiline=False)
+    draw_text(draw,  "GDG PUP " + "Cybersecurity " + "Lead", 336.55, 555.96, 107.91, 8, "medium", multiline=False)
+    draw_text(draw,  colead_name, 501, 537, 121, 13, "bold", multiline=False)
+    draw_text(draw,  "GDG PUP " + "Cybersecurity " + "Co-Lead", 500, 554, 122, 8, "medium", multiline=False)
+
+    # Return the image
+    return img
