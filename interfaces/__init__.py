5836349e9fd885b0bfeee157761bae2a2d67cf60
JpCurada <johncurada.02@gmail.com>
2025-03-10 14:17:42 +0800

    remove __init__.py as it will no longer be used

--- interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-from pages.home import show_home
-from pages.events import show_events
-from pages.certificates import show_certificates
-from pages.xparky import show_xparky
-from pages.submission import show_submission
