cfb1090e3adf4ab165101dc758218fecd7589ac7
Jen Patrick Nataba <150793733+cytojen@users.noreply.github.com>
2025-03-17 23:12:50 +0800

    style: fixed date alignment for cards

--- interfaces/events.py
+++ interfaces/events.py
@@ -280,7 +280,12 @@ def show_past_events(df):
                             st.markdown(f"<div class='event-title'>{event['title']}</div>", unsafe_allow_html=True)
                             st.image("static/images/gdg_card.png", use_container_width=False)
                             st.markdown(f"<div class='event-date'>{event['datetime']}</div>", unsafe_allow_html=True)
-                            
+                            # Check if the title is shorter than or equal to 40 characters, and if so, add a newline before the date
+                            if len(event['title']) <= 41:
+                                st.markdown(f"<div class='event-date'><br>{event['datetime']}</div>", unsafe_allow_html=True)
+                            else:
+                                st.markdown(f"<div class='event-date'>{event['datetime']}</div>", unsafe_allow_html=True)
+                                
                             # Button last (higher z-index)
                             # This empty space ensures the button is rendered after the content
                             st.markdown("<div style='height: 1px;'></div>", unsafe_allow_html=True)
